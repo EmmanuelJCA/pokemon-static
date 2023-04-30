147192e7ef48d9c1317e660a702504ed371b7a44
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-04-30 16:01:50 -0400

    Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,35 @@
-import { NextPage } from "next"
+import { NextPage, GetStaticProps } from "next"
+import { Layout } from "@/components/layouts"
+import { pokeApi } from "@/api"
 
 
 const HomePage: NextPage = () => {
   return (
-    <>
-      <h1>Pokemon Static</h1>
-    </>
+    <Layout>
+      <ul>
+        <li>pokemon</li>
+        <li>pokemon</li>
+        <li>pokemon</li>
+        <li>pokemon</li>
+        <li>pokemon</li>
+        <li>pokemon</li>
+        <li>pokemon</li>
+      </ul>
+    </Layout>
   )
+}
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+  const { data } = await pokeApi.get('/pokemon?limit=151')  
+
+  return {
+    props: {
+      pokemons: data
+    }
+  }
+  
 }
 
 export default HomePage
