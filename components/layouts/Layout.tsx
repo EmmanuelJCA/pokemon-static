35f7d2df78684a2078074942033f1096ac619a57
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-05-06 18:54:21 -0400

    meta og for the SEO

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -6,14 +6,20 @@ interface Props {
   title?: string
 }
 
+const origin = (typeof window == 'undefined') ? '' : window.location
+
 export const Layout: FC<PropsWithChildren<Props>> = ({ children, title }) => {
+
   return (
     <>
       <Head>
         <title>{ title || 'PokemonApp' }</title>
         <meta name="author" content="Emmanuel Cañate" />
-        <meta name="description" content="Information about the pokemon ******" />
+        <meta name="description" content={`Information about the pokemon ${title}`} />
         <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+        <meta property="og:title" content={`Informacion sobre ${ title }`} />
+        <meta property="og:description" content={`Esta es la pagina sobre ${ title }`} />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
       </Head>
       <Navbar />
       <main>
--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -4,13 +4,14 @@ import { Button, Card, Container, Grid, Image, Text } from '@nextui-org/react'
 import confetti from 'canvas-confetti'
 import { Layout } from '@/components/layouts'
 import { getPokemonInfo, localFavorites } from '@/utils'
-import { Pokemon } from '@/interfaces'
+import { Pokemon, PokemonListResponse } from '@/interfaces'
+import { pokeApi } from '@/api'
 
 interface Props {
   pokemon: Pokemon
 }
 
-const PokemonPage: NextPage<Props> = ({ pokemon }) => {  
+const PokemonByNamePage: NextPage<Props> = ({ pokemon }) => {  
 
   const [isInFavorites, setIsInFavorites] = useState(localFavorites.verifyFavorites( pokemon.id ))
 
@@ -102,25 +103,27 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
-  const totalPokemons = [...Array(151)].map( ( value, index) => `${ index+1 }`)
-  
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+  const pokemonNames: string[] = data.results.map( pokemon => pokemon.name );
+
+
   return {
-    paths: totalPokemons.map( id => ({
-      params: { id }
+    paths: pokemonNames.map( name => ({
+      params: { name }
     })),
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
 
-  const { id } = params as { id: string }
+  const { name } = params as { name: string }
 
   return {
     props: {
-      pokemon: await getPokemonInfo(id)
+      pokemon: await getPokemonInfo(name)
     }
   }
 }
 
-export default PokemonPage
+export default PokemonByNamePage
