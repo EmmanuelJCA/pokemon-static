92f89914d1fd3cd2ec9c9daee9209e56c04c28e0
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-05-06 20:35:29 -0400

    Implement ISG

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -119,10 +119,22 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   const { name } = params as { name: string }
 
+  const pokemon = await getPokemonInfo(name.toLowerCase())
+
+  if(!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(name)
-    }
+      pokemon
+    },
+    revalidate: 86400
   }
 }
 
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -5,6 +5,7 @@ import confetti from 'canvas-confetti'
 import { Layout } from '@/components/layouts'
 import { getPokemonInfo, localFavorites } from '@/utils'
 import { Pokemon } from '@/interfaces'
+import { redirect } from 'next/dist/server/api-utils'
 
 interface Props {
   pokemon: Pokemon
@@ -108,7 +109,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: totalPokemons.map( id => ({
       params: { id }
     })),
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
@@ -116,10 +117,22 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   const { id } = params as { id: string }
 
+  const pokemon = await getPokemonInfo(id)
+
+  if(!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(id)
-    }
+      pokemon
+    },
+    revalidate: 86400
   }
 }
 
